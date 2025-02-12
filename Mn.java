91b0fc6d8c70e6fc96e258d045b2247d70b1d9e6
Anusharao-08 <anusharao8105@gmail.com>
2025-02-12 07:42:06 +0530

    Add files via upload

---
+++ Mn.java
@@ -0,0 +1,17 @@
+public class mn {
+    public static void main(String[] args) {
+        TradingAccount account = new TradingAccount(10000.0);
+
+        Stock stock1 = new Stock("AAPL", 150.0);
+        Stock stock2 = new Stock("GOOG", 2500.0);
+
+        account.buyStock(stock1, 10);
+        account.buyStock(stock2, 5);
+
+        account.printPortfolio();
+
+        account.sellStock(stock1, 5);
+
+        account.printPortfolio();
+    }
+}
